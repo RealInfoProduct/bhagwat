8c193618a084bb010b7fce46fe72b0fe00e26a5d
RealInfoProduct <reallocinfotechproduct@gmail.comt>
2026-04-03 17:30:52 +0530

    environment chang fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,28 @@
 export const environment = {
 
-    production: true,
+  production: false,
 
-      firebaseConfig : {
-  apiKey: "AIzaSyB7WwxGbXoxi-2Ol9H_3_Dk0WWnJIG6xWE",
-  authDomain: "bhagwat-5d4b7.firebaseapp.com",
-  projectId: "bhagwat-5d4b7",
-  storageBucket: "bhagwat-5d4b7.firebasestorage.app",
-  messagingSenderId: "51302759935",
-  appId: "1:51302759935:web:f116098147b183214e7bca",
-  measurementId: "G-H2D81DX8TS"
+  firebaseConfig: {
+    apiKey: "AIzaSyB7WwxGbXoxi-2Ol9H_3_Dk0WWnJIG6xWE",
+    authDomain: "bhagwat-5d4b7.firebaseapp.com",
+    projectId: "bhagwat-5d4b7",
+    storageBucket: "bhagwat-5d4b7.firebasestorage.app",
+    messagingSenderId: "51302759935",
+    appId: "1:51302759935:web:f116098147b183214e7bca",
+    measurementId: "G-H2D81DX8TS"
+  }
 }
-}
+// export const environment = {
+
+//   production: false,
+
+//   firebaseConfig: {
+//     apiKey: "AIzaSyB7WwxGbXoxi-2Ol9H_3_Dk0WWnJIG6xWE",
+//     authDomain: "bhagwat-5d4b7.firebaseapp.com",
+//     projectId: "bhagwat-5d4b7",
+//     storageBucket: "bhagwat-5d4b7.firebasestorage.app",
+//     messagingSenderId: "51302759935",
+//     appId: "1:51302759935:web:f116098147b183214e7bca",
+//     measurementId: "G-H2D81DX8TS"
+//   }
+// }
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,28 @@
 export const environment = {
-  
-    production: false,
+
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyCXBdfZZor_gZ3QRrlvGFB3Hu3E-pC8g5A",
+        authDomain: "bhagwatdev-99a03.firebaseapp.com",
+        projectId: "bhagwatdev-99a03",
+        storageBucket: "bhagwatdev-99a03.firebasestorage.app",
+        messagingSenderId: "1025995579278",
+        appId: "1:1025995579278:web:5efa0780d95e19dd024edf",
+        measurementId: "G-YV2TM8FR37"
+    }
 
 }
+// export const environment = {
+
+//     production: true,
+//     firebaseConfig: {
+//         apiKey: "AIzaSyCXBdfZZor_gZ3QRrlvGFB3Hu3E-pC8g5A",
+//         authDomain: "bhagwatdev-99a03.firebaseapp.com",
+//         projectId: "bhagwatdev-99a03",
+//         storageBucket: "bhagwatdev-99a03.firebasestorage.app",
+//         messagingSenderId: "1025995579278",
+//         appId: "1:1025995579278:web:5efa0780d95e19dd024edf",
+//         measurementId: "G-YV2TM8FR37"
+//     }
+
+// }
